106a2a86b03834aefdb2fb85bfd5632bcae5efc4
Johnny Galindo <John_galindo@hotmail.com>
2021-02-13 08:27:52 -0600

    init

---
+++ WebApplication13/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication13.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 159px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server" Width="508px">DefaultEndpointsProtocol=https;AccountName=johngalvirustest;AccountKey=2gEBhj77u7TNQVetl03eiG5+nWT5Husd80vFY+O8Dyj1crbrnTI89mNlikKoTLiU4EkNm0DDQUe4ZCqcPqPIsA==;EndpointSuffix=core.windows.net</asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td>
+                        <asp:TextBox ID="TextBox2" runat="server" Width="508px">uploads</asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td>
+                        <asp:TextBox ID="TextBox3" runat="server" Width="505px">w3wp.exe_210120_162545.dmp</asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Button" />
+    </form>
+</body>
+</html>
---
+++ WebApplication13/WebForm1.aspx.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication13
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string storageConnectionString = TextBox1.Text;
+            CloudStorageAccount storageAccount;
+
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(TextBox2.Text);
+
+                string Dir_path = Server.MapPath("~/tmp//");
+                string name = TextBox3.Text;
+                string newname = DateTimeOffset.Now.UtcTicks.ToString();
+
+                CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference(name);
+                    string path = (Dir_path + newname);
+                    blockBlob.DownloadToFile(path, FileMode.Create);
+                 //Response.WriteFile(path);
+
+                FileInfo file = new FileInfo(path);
+
+                Response.Clear();
+                Response.ClearHeaders();
+                Response.ClearContent();
+
+                Response.AddHeader("Content-Disposition", "attachment; filename="+ newname);
+                Response.AddHeader("Content-Length", file.Length.ToString());
+                Response.ContentType = "text/plain";
+                Response.Flush();
+                Response.WriteFile(file.FullName);
+                try
+                {
+
+                
+                Response.TransmitFile(path);
+                Response.End();
+                File.Delete(path);
+                }
+                catch(Exception ex)
+                {
+                    Response.Write(ex.Message);
+                }
+            }
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string storageConnectionString = TextBox1.Text;
+            CloudStorageAccount storageAccount;
+
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+
+                CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(TextBox2.Text);
+
+                string Dir_path = Server.MapPath("~/tmp//");
+                string name = TextBox3.Text;
+                string newname = DateTimeOffset.Now.UtcTicks.ToString();
+
+                CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference(name);
+                string path = (Dir_path + newname);
+                blockBlob.DownloadToFile(path, FileMode.Create);
+               
+
+                HttpContext.Current.Response.ContentType = "application/octet-stream";
+                HttpContext.Current.Response.AddHeader("Content-Disposition", "attachment; filename=\"" + name + "\"");
+                HttpContext.Current.Response.Clear();
+                HttpContext.Current.Response.WriteFile(Dir_path + newname);
+                HttpContext.Current.Response.Flush();
+                File.Delete(Dir_path + newname);
+                HttpContext.Current.Response.End();
+            }
+        }
+    }
+}
